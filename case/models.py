a24fc660d224f05181c9c32e0af38c1375dc6d84
name <ksyusha0kubay@gmail.com>
2023-04-03 10:58:09 +0300

    Add models to project

--- case/models.py
+++ case/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField(max_length=63)
+    created_time = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    def __str__(self) -> str:
+        return f"{self.content}, created_time: {self.created_time}, deadline: {self.deadline}"
