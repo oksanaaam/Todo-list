46712c99520d5e508ce202effd142255fad75176
name <ksyusha0kubay@gmail.com>
2023-04-03 14:02:24 +0300

    Add Create Update & Delete for views

--- case/urls.py
+++ case/urls.py
@@ -3,19 +3,39 @@ from case.views import (
     index,
     TagListView,
     TagDetailView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
     TaskListView,
-    TaskDetailView
+    TaskDetailView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("tags/", TagListView.as_view(), name="tag-list"),
-    path("tags/<int:pk>/", TagDetailView.as_view(), name="tag-detail-list"),
+    path("tags/<int:pk>/", TagDetailView.as_view(), name="tag-detail"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path(
         "tasks/<int:pk>/",
         TaskDetailView.as_view(),
-        name="task-detail-list"
+        name="task-detail"
+    ),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update",
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete",
     ),
 ]
 
--- case/views.py
+++ case/views.py
@@ -2,6 +2,7 @@ from urllib.request import Request
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from case.models import Tag, Task
@@ -32,11 +33,49 @@ class TagDetailView(generic.DetailView):
     model = Tag
 
 
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    template_name = "case/tag_form.html"
+    success_url = reverse_lazy("case:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    template_name = "case/tag_form.html"
+    success_url = reverse_lazy("case:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    template_name = "case/tag_confirm_delete.html"
+    success_url = reverse_lazy("case:tag-list")
+
+
 class TaskListView(generic.ListView):
     model = Task
     paginate_by = 5
 
 
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("case:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("case:task-list")
+
+
 class TaskDetailView(generic.DetailView):
     model = Task
     queryset = Task.objects.prefetch_related("tags__tasks")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("case:task-list")
+
